1ea0c95bb0da6b8bf95d429e1fcaa7d37f76bb1e
ArthurYang80 <arthur.yang@student.tdsb.on.ca>
2026-02-10 21:28:09 +0000

    Completed

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -12,33 +12,69 @@ public class PracticeProblem {
 	}
 
 	public static void q1() {
-		//Write question 1 code here
-		
+	Scanner s = new Scanner(System.in);
+		System.out.println("Input an integer: ");
+	String num = s.nextLine();
+	num = "5" + num;
+	int word = Integer.parseInt(num);
+	word += 5;
+	System.out.println(word);
+	s.close();
 	}
 
 	public static void q2() {
-		//Write question 2 code here
-		
+	Scanner s = new Scanner(System.in);
+		System.out.println("Input a number: ");	
+	String num = s.nextLine();
+	num = "4" + num + "3";
+	double dub = Double.parseDouble(num);
+	dub += 3.4;
+	System.out.println(dub);
+	s.close();	
 	}
 
 	public static void q3() {
-		//Write question 3 code here
-		
+	Scanner s = new Scanner(System.in);
+		System.out.println("Input a boolean: ");
+	String num = s.nextLine();
+	boolean bool = Boolean.parseBoolean(num); 
+	bool = !bool;
+	System.out.println(bool);
+	s.close();
 	}
 
 	public static void q4() {
-		//Write question 4 code here
-		
+	Scanner s = new Scanner(System.in);
+	System.out.println("Input an integer: ");
+	String num = s.nextLine();
+	num = num + "3";
+	int value = Integer.parseInt(num);
+	value += 2;
+	char newValue = (char)value; 
+	System.out.println(newValue);
+	s.close();
 	}
 
 	public static void q5() {
-		//Write question 5 code here
-		
+	Scanner s = new Scanner(System.in);
+	System.out.println("Input an integer: ");
+	String num = s.nextLine();
+	num = num + "1";
+	int word = Integer.parseInt(num);
+	double dub = word / 2.0;
+	System.out.println(dub);
+	s.close();	
 	}
 
 	public static void q6() {
-		//Write question 6 code here
-		
+	Scanner s = new Scanner(System.in);
+	System.out.println("Input an number: ");
+	String num = s.nextLine();
+	num = "1" + num;
+	double word = Double.parseDouble(num);
+	int word2 = (int)word;
+	System.out.println(word2);
+	s.close();
 	}
 
 }
